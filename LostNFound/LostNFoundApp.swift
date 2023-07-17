c2d384ecb106b183fd481f71c9e10a14ba581050
Gregorius Yuristama <102383943+gregoriusyuristama@users.noreply.github.com>
2023-07-17 11:18:07 +0700

    Initial Commit

---
+++ LostNFound/LostNFoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostNFoundApp.swift
+//  LostNFound
+//
+//  Created by Gregorius Yuristama Nugraha on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LostNFoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
