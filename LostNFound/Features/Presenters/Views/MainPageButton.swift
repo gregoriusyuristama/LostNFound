29f3bdde1997412c3a703c0e093d3305979553ed
dkhairanb <93834247+dkhairanb@users.noreply.github.com>
2023-07-21 16:45:53 +0700

    Main Page Menu Done (#5)
    Co-authored-by: Gregorius Yuristama <102383943+gregoriusyuristama@users.noreply.github.com>

---
+++ LostNFound/Features/Presenters/Views/MainPageButton.swift
@@ -0,0 +1,36 @@
+//
+//  MainPageButton.swift
+//  LostNFound
+//
+//  Created by Dhil Khairan Badjiser on 21/07/23.
+//
+
+import SwiftUI
+
+struct MainPageButton: View {
+    
+    @State var buttonName: String = ""
+    
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .frame(width: UIScreen.main.bounds.width*0.17423, height: UIScreen.main.bounds.height*0.16797)
+                .foregroundColor(Color.white)
+                .cornerRadius(30)
+                .shadow(radius: 4, x: 8, y: 8)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+
+            Image("\(buttonName)")
+        }
+    }
+}
+
+struct MainPageButton_Previews: PreviewProvider {
+    static var previews: some View {
+        MainPageButton(buttonName: "found_items")
+            .previewInterfaceOrientation(.landscapeLeft)
+    }
+}
