bcc0a6f89ceb67d22f9a948050628f062aa4e375
Tiff12 <tiffanyindryani@gmail.com>
2023-07-23 20:48:47 +0700

    string matching frontend (#11)

--- LostNFound/Features/Presenters/Views/LostItemNonImageView.swift
+++ LostNFound/Features/Presenters/Views/LostItemNonImageView.swift
@@ -14,88 +14,114 @@ struct ImageDescription: Hashable{
 
 struct LostItemNonImageView: View {
     let imageDescriptions: [ImageDescription] = [
-            ImageDescription(imageName: "JaketBiru1", description: "Jaket Biru"),
-            ImageDescription(imageName: "JaketBiru2", description: "Jaket Biru"),
-            ImageDescription(imageName: "JaketBiru3", description: "Jaket Biru"),
-            ImageDescription(imageName: "JaketMerah1", description: "Jaket Merah"),
-            ImageDescription(imageName: "JaketMerah2", description: "Jaket Merah"),
-            ImageDescription(imageName: "JaketMerah3", description: "Jaket Merah"),
-            ImageDescription(imageName: "JaketHijau1", description: "Jaket Hijau"),
-            ImageDescription(imageName: "JaketHijau2", description: "Jaket Hijau"),
-            ImageDescription(imageName: "JaketKuning", description: "Jaket Kuning"),
-            ImageDescription(imageName: "JaketUngu", description: "Jaket Ungu"),
-        ]
+        ImageDescription(imageName: "JaketBiru1", description: "Jaket Biru"),
+        ImageDescription(imageName: "JaketBiru2", description: "Jaket Biru"),
+        ImageDescription(imageName: "JaketBiru3", description: "Jaket Biru"),
+        ImageDescription(imageName: "JaketMerah1", description: "Jaket Merah"),
+        ImageDescription(imageName: "JaketMerah2", description: "Jaket Merah"),
+        ImageDescription(imageName: "JaketMerah3", description: "Jaket Merah"),
+        ImageDescription(imageName: "JaketHijau1", description: "Jaket Hijau"),
+        ImageDescription(imageName: "JaketHijau2", description: "Jaket Hijau"),
+        ImageDescription(imageName: "JaketKuning", description: "Jaket Kuning"),
+        ImageDescription(imageName: "JaketUngu", description: "Jaket Ungu"),
+    ]
     let itemDesc = "Jaket biru tua dengan zipper"
     var body: some View {
         VStack{
-            BlueTitle()
+            //            BlueTitle()
             HStack{
-                ItemImage(imageName: "NoImage").padding(70)
-                VStack(alignment: .leading){
-                    HStack{
-                        VStack(alignment: .leading){
+                ItemImage(imageName: "NoImage").padding(48)
+                VStack{
+                    HStack(alignment: .top){
+                        VStack(alignment: .leading, spacing: 17){
                             Text("Jaket Hilang")
-                                .font(.system(size: 50))
+                                .font(.custom(AppFonts.helveticaNeue, size: 25))
                                 .bold()
-                            
                             Text(itemDesc)
-                                .font(.system(size: 30))
+                                .font(.custom(AppFonts.helveticaNeue, size: 20))
                         }
-                        VStack{
-                            ProcessButton()
-                        }
+                        Spacer()
+                        ProcessButton()
+                            .padding(.leading, 30)
                     }
-                    Text("Item Description")
-                        .font(.system(size: 50))
-                        .bold()
-                    VStack(alignment: .leading){
-                        HStack{
-                            Text("Category")
-                                .font(.system(size: 30))
+                    .frame(height: UIScreen.main.bounds.height * 0.157)
+                    HStack {
+                        VStack(alignment: .leading){
+                            Text("Item Description")
+                                .font(.custom(AppFonts.helveticaNeue, size: 25))
                                 .bold()
-                            Text("Non Valuable")
-                                .font(.system(size: 30))
+                                .padding(.bottom, 17)
+                            HStack{
+                                Text("Category")
+                                    .bold()
+                                Spacer()
+                                Text("Non Valuable")
+                            }
+                            Divider()
+                            HStack{
+                                Text("Location")
+                                    .bold()
+                                Spacer()
+                                Text("MRT Bundaran HI")
+                            }
+                            Divider()
+                            HStack{
+                                Text("Lost Date")
+                                    .bold()
+                                Spacer()
+                                Text("17 July 2023")
+                            }
                         }
-                        HStack{
-                            Text("Location")
-                                .font(.system(size: 30))
-                                .bold()
-                            Text("MRT Bundaran HI")
-                                .font(.system(size: 30))
-                        }
-                        HStack{
-                            Text("Lost Date")
-                                .font(.system(size: 30))
-                                .bold()
-                            Text("17 July 2023")
-                                .font(.system(size: 30))
-                        }
+                        .font(.custom(AppFonts.helveticaNeue, size: 18))
+                        .frame(width: UIScreen.main.bounds.width * 0.382, height: UIScreen.main.bounds.height * 0.157)
+                        Spacer()
                     }
                 }
             }
+            Divider()
             HStack{
                 Spacer()
                 Text("Match Item")
-                    .font(.system(size: 50))
+                    .font(.custom(AppFonts.helveticaNeue, size: 30))
                     .bold()
                 Spacer()
                 Text("3 out of 10")
-                    .font(.system(size: 20))
+                    .font(.custom(AppFonts.helveticaNeue, size: 15))
+                    .bold()
+                    .padding(.trailing, 20)
             }
-            HStack {
-                ForEach(imageDescriptions.filter { isWordInSentence($0.description, itemDesc) }, id: \.self) { imageDescription in
+            ScrollView(.vertical){
+                LazyVGrid(columns: gridItems) {                ForEach(imageDescriptions.filter { isWordInSentence($0.description, itemDesc) }, id: \.self) { imageDescription in
                     VStack{
                         Image(imageDescription.imageName)
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 100, height: 100)
+                            .frame(width: 200, height: 200)
                         Text(imageDescription.description)
                             .font(.headline)
                             .padding()
                     }
                 }
+                }
             }
+            Spacer()
         }
+        .navigationBarTitle("")
+        .toolbar(content: {
+            ToolbarItem(id: "title", placement: .navigationBarLeading) {
+                Text("Lost #88")
+                    .foregroundColor(.white)
+                    .font(.custom(AppFonts.helveticaNeue, size: 50))
+                    .bold()
+                    .padding(.top, 17)
+                    .padding(.leading, 17)
+            }
+        })
+        .toolbarBackground(AppColor.blueNavigationBar, for: .navigationBar)
+        .toolbarBackground(.visible, for: .navigationBar)
+    }
+    var gridItems: [GridItem] {
+        Array(repeating: GridItem(.flexible(), spacing: 16), count: 3)
     }
 }
 
